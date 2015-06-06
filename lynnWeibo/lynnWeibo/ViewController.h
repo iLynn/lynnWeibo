ed8c8340a02338e156227b08696e2fd6fc58bd61
iLynn <whucslynn@163.com>
2015-06-06 14:30:58 +0800

    new lynnWeibo project

---
+++ lynnWeibo/lynnWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lynnWeibo
+//
+//  Created by Lynn on 15/6/6.
+//  Copyright (c) 2015年 Lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
